d82e1f23557770bf8cc4f34f423d717d95040553
Nick Janne <njanne@umich.edu>
2024-08-29 16:03:30 -0400

    Add: containerization

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,19 +1,24 @@
-export default ({ env }) => ({
-    upload: {
-        config: {
-            provider: 'aws-s3',
-            providerOptions: {
-                s3Options: {
-                    credentials: {
-                        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                        secretAccessKey: env('AWS_ACCESS_SECRET'),
-                    },
-                    region: env('AWS_REGION'),
-                    params: {
-                      Bucket: env('AWS_BUCKET_NAME'),
-                    },
-                }
+export default ({ env }) => {
+
+    const useS3 = env('USE_S3', false);
+
+    return {
+        upload: useS3 ? {
+            config: {
+                provider: 'aws-s3',
+                providerOptions: {
+                    s3Options: {
+                        credentials: {
+                            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                            secretAccessKey: env('AWS_ACCESS_SECRET'),
+                        },
+                        region: env('AWS_REGION'),
+                        params: {
+                          Bucket: env('AWS_BUCKET_NAME'),
+                        },
+                    }
+                },
             },
-        },
+        } : {},
     }
-});
+};
